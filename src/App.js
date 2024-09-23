6ab733217b0eea08f3c4b0c42306e17f984084ba
Gustavo Pasquini <99883235+GustavoPasquiniLucas@users.noreply.github.com>
2024-09-23 09:47:45 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className='App-header'>
+        <div className='logo'>
+          <img src={logo} alt='logo'></img>
+          <p><strong>Store</strong>Books</p>
+        </div>
+      </header>
+    </div>
+  )
+}
+
+export default App;
+
